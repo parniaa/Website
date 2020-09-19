d53b3f2cf7d9922316a58b1aecae8f8e9407fbcd
parniaa <parniaa@gmail.com>
2020-09-18 23:44:40 -0400

    k

--- GameProject/JS/src.js
+++ GameProject/JS/src.js
@@ -6,6 +6,23 @@ let bothDoorPath = " https://s3.amazonaws.com/codecademy-content/projects/chore-
 let beachDoorPath = " https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg ";
 let spaceDoorPath = " https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg ";
 
+let numClosedDoors = 3 ;
+let openDoor1 ;
+let openDoor2 ;
+let openDoor3 ;
+
+
+
+const randomChoreDoorGenerator = () => {
+    var chorDoor = Math.floor(Math.random) * numClosedDoors
+    if (chorDoor === 0) {
+        openDoor1 = bothDoorPath;
+    } else if (chorDoor === 1) { 
+        openDoor2 =bothDoorPath;        
+    }
+    
+  }
+
 doorImage1.onclick = () => {
 
   doorImage1.src = bothDoorPath;
