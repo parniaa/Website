b48343eee15a15ad3683f89a826c78260e7a380e
parniaa <parniaa@gmail.com>
2020-09-21 14:46:07 -0400

    j

--- GameProject/JS/src.js
+++ GameProject/JS/src.js
@@ -5,35 +5,74 @@ let doorImage3 = document.getElementById("door3");
 let bothDoorPath = " https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg ";
 let beachDoorPath = " https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg ";
 let spaceDoorPath = " https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg ";
+let closedDoorPath = " https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg ";
 
 let numClosedDoors = 3 ;
 let openDoor1 ;
 let openDoor2 ;
 let openDoor3 ;
 
+const isClicked = (door) => {
 
+}
+
+const playDoor = () => {
+
+    numClosedDoors--;
+    if (numClosedDoors === 0) {
+        gameOver();
+    } else {
+        
+    }
+}
 
 const randomChoreDoorGenerator = () => {
-    var chorDoor = Math.floor(Math.random) * numClosedDoors
+
+    var chorDoor = Math.floor(Math.random() * numClosedDoors);
+    
+    
     if (chorDoor === 0) {
+
         openDoor1 = bothDoorPath;
+
+        openDoor2 = beachDoorPath;
+
+        openDoor3 = spaceDoorPath;
+
     } else if (chorDoor === 1) { 
-        openDoor2 =bothDoorPath;        
+
+        openDoor2 = bothDoorPath;
+
+        openDoor1 = beachDoorPath;
+
+        openDoor3 = spaceDoorPath;  
+
+    } else {
+
+        openDoor3 = bothDoorPath;
+
+        openDoor2 = beachDoorPath;
+
+        openDoor1 = spaceDoorPath;
     }
     
   }
 
 doorImage1.onclick = () => {
 
-  doorImage1.src = bothDoorPath;
+  doorImage1.src = openDoor1;
+  playDoor();
   
 }
 doorImage2.onclick = () => {
 
-    doorImage2.src = beachDoorPath;
-
+    doorImage2.src = openDoor2;
+    playDoor();
 }
 doorImage3.onclick = () => {
     
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
+    playDoor();
 }
+randomChoreDoorGenerator();
+
